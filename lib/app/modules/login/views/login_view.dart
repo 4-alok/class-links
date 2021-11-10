12b3b18ef876badf74334e350b3136b4573dc5b7
lzzy12 <jhashivam2020@gmail.com>
2021-11-10 11:45:22 +0530

    Call google login method
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>
    
    # Conflicts:
    #	lib/main.dart

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -12,64 +12,72 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: ThemeClass.buildTheme(context).primaryColor,
-        body: Container(
-          padding: EdgeInsets.only(top: 50),
-          child: SafeArea(
-            child: SingleChildScrollView(
-              child: Center(
-                child: Column(
-                  children: [
-                    Text(
-                      'Welcome',
+      backgroundColor: ThemeClass.buildTheme(context).primaryColor,
+      body: Container(
+        padding: EdgeInsets.only(top: 50),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  Text(
+                    'Welcome',
+                    style: ThemeClass.buildTheme(context)
+                        .textTheme
+                        .headline1!
+                        .copyWith(fontSize: 50, color: Color(0xff510481)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10),
+                    child: Text(
+                      'Making College life easy',
                       style: ThemeClass.buildTheme(context)
                           .textTheme
-                          .headline1!
-                          .copyWith(fontSize: 50, color: Color(0xff510481)),
+                          .bodyText1!
+                          .copyWith(fontSize: 25, color: Color(0xff510481)),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10, bottom: 10),
-                      child: Text(
-                        'Making College life easy',
-                        style: ThemeClass.buildTheme(context)
-                            .textTheme
-                            .bodyText1!
-                            .copyWith(fontSize: 25, color: Color(0xff510481)),
-                      ),
-                    ),
-                    SvgPicture.asset(
-                      'images/bgimg.svg',
-                      width: 600,
-                    ),
-                    GestureDetector(
-                      onTap: () async {
-                        final service = Get.find<AuthService>();
+                  ),
+                  SvgPicture.asset(
+                    'images/bgimg.svg',
+                    width: 600,
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      final service = Get.find<AuthService>();
+                      try {
                         await service.login();
-                        Get.to(SectionSelectionView());
-                      },
-                      child: Container(
-                        height: 50,
-                        width: 250,
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              SvgPicture.asset('images/googlelogo.svg'),
-                              Text("Sign in with Google",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20))
-                            ]),
-                        decoration: BoxDecoration(
-                          color: Color(0xff510481),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(
+                          e.toString(),
+                        )));
+                        return;
+                      }
+                      Get.to(SectionSelectionView());
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 250,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            SvgPicture.asset('images/googlelogo.svg'),
+                            Text("Sign in with Google",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20))
+                          ]),
+                      decoration: BoxDecoration(
+                        color: Color(0xff510481),
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  )
+                ],
               ),
             ),
           ),
         ),
-      );
+      ),
+    );
   }
 }
