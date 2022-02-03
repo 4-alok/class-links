bcc7a165c95ac2242c3efe391c84270568f9de24
Alok Kumar <kumaralok344@gmail.com>
2022-02-03 09:38:36 +0530

    Updated support mail

--- lib/app/global/const/app_info.dart
+++ lib/app/global/const/app_info.dart
@@ -3,7 +3,7 @@ class AppInfo {
   static const String appVersion = "v0.1.1";
   static const String applicationLegalese =
       "Copyright (c) 2024 GDSC TEAM KIIT" "\nMIT License";
-  static const String supportMail = "classlink@dsckiit.in";
+  static const String supportMail = "classlinks@dsckiit.in";
   static const String appInfo = "Open class-link with a single click. "
       "Easily sync the same batch's schedule and class-link.\n\n";
   static const String github = "https://github.com/DSC-KIIT/class-links";
